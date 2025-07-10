d02c151b9842dacb4cc009807454355336f40f34
kuruneru <kuruneru.ikkou@gmail.com>
2025-07-10 15:37:38 +0900

    Docker_and_MySQL

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from sqlalchemy import create_engine, text
+import os
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+DB_URL = f"mysql+pymysql://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}:3306/{os.environ['DB_NAME']}"
+engine = create_engine(DB_URL)
 
 @app.get("/", response_class=HTMLResponse)
 def read_root(request: Request):
