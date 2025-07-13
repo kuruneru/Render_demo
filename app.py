5bb5b4c1b8aa07f66df2c1dc04d290cb3c2f332d
kuruneru <kuruneru@yamadatakayuki.localdomain>
2025-07-14 02:06:04 +0900

    debug_Neon

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI, Request
 from sqlalchemy import create_engine, text
 from starlette.templating import Jinja2Templates
+from dotenv import load_dotenv  # ← これを追加
 import os
+
+load_dotenv()  # ← .env ファイルの読み込み
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-db_url = os.getenv("DATABASE_URL")
+db_url = os.getenv("DATABASE_URL")  # Noneになる原因はこれ
 engine = create_engine(db_url, echo=True)
 
 @app.get("/")
